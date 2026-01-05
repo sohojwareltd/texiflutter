825abfe37fe5e73de5bd446a951e6664356c5c7a
Uzzal Biswas <149400155+ubwasi@users.noreply.github.com>
2026-01-05 16:28:31 +0600

    hi

--- lib/components/text_fields/input_Text_field.dart
+++ lib/components/text_fields/input_Text_field.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 class InputTextField extends StatelessWidget {
-  const InputTextField({super.key});
+  String hint;
+  TextEditingController controller;
+ InputTextField(
+      {super.key, required this.hint, required this.controller});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hint,
+        filled: true,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: Colors.grey),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(color: Colors.blue),
+        ),
+      ),
+    );
+
   }
 }
