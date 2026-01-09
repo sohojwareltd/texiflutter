574aa199da9104010cd3ca9416f20d9442ebcccf
Uzzal Biswas <149400155+ubwasi@users.noreply.github.com>
2026-01-09 18:32:17 +0600

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:taxiflutter/screens/for_Hire.dart';
 import 'package:taxiflutter/screens/login_screens.dart';
 import 'package:taxiflutter/screens/menu_screen.dart';
 import 'package:taxiflutter/screens/permission_required.dart';
+import 'package:taxiflutter/screens/qr_scan_screen.dart';
 import 'package:taxiflutter/screens/taxi_Meter.dart';
 import 'package:taxiflutter/screens/taxi_Receipt_Screen.dart';
 import 'components/Payment_Components/current_Fare.dart';
@@ -43,7 +44,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: CardPay()
+      home: QrScanScreen()
     );
   }
 }
